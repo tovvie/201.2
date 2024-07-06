a1fd30f1462d68e86e190ce15fe2581e2a252fe5
ashiemac <ashiemcanally@gmail.com>
2024-07-06 23:04:15 +1200

    jump animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed; // allowing to edit in unity 
     private Rigidbody2D body;
     private Animator anim;
+    private bool grounded;
 
     private void Awake()
     {
@@ -29,10 +30,24 @@ public class PlayerMovement : MonoBehaviour
         else if (horizontalInput < -0.01f)
             transform.localScale = new Vector3(-1, 1, 1);
 
-        if (Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
 
         //set animator parameters
         anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
     }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+            grounded = true;
+    }
+
 }
